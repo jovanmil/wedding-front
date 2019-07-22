2f5efa6015cec8d133c1d92db7178bb635bb9484
jovan <jovan.milutinovic84@gmail.com>
2019-07-22 08:55:39 +0200

    Initial table added

--- src/main/Home.js
+++ src/main/Home.js
@@ -7,61 +7,63 @@ import {POPULATE_KEY_LOG_IN, TYPE_LOG_IN} from "../redux/actions/constants";
 
 class Home extends Component {
 
-    col_md_12 = {
-        alignItems: "center",
-        textAlign: "center"
-    };
+  col_md_12 = {
+    alignItems: "center",
+    textAlign: "center",
+    color: "red",
+    fontSize: "18px"
+  };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: ""
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
     }
+  }
 
-    nextPath() {
-        const {
-            history
-        } = this.props;
+  nextPath() {
+    const {
+      history
+    } = this.props;
 
-        history.push("/guests");
+    history.push("/guests");
+  }
+
+  handleClick() {
+    const {
+      doLogin
+    } = this.props;
+
+    doLogin(this.state, POPULATE_KEY_LOG_IN, TYPE_LOG_IN);
+  }
+
+  handleKeyPress = (e) => {
+    const {
+      doLogin
+    } = this.props;
+
+    if (e.key === "Enter") {
+      doLogin(this.state, POPULATE_KEY_LOG_IN, TYPE_LOG_IN);
     }
+  };
 
-    handleClick() {
-        const {
-            doLogin
-        } = this.props;
+  updatePassword(event) {
+    this.setState({password: event.target.value})
+  }
 
-        doLogin(this.state, POPULATE_KEY_LOG_IN, TYPE_LOG_IN);
-    }
+  updateEmail(event) {
+    this.setState({email: event.target.value})
+  }
 
-    handleKeyPress = (e) => {
-        const {
-            doLogin
-        } = this.props;
+  render() {
+    const {
+      authToken
+    } = this.props;
 
-        if (e.key === "Enter") {
-            doLogin(this.state, POPULATE_KEY_LOG_IN, TYPE_LOG_IN);
-        }
-    };
-
-    updatePassword(event) {
-        this.setState({password: event.target.value})
-    }
-
-    updateEmail(event) {
-        this.setState({email: event.target.value})
-    }
-
-    render() {
-        const {
-            authToken
-        } = this.props;
-
-        const formJsx = [];
-        formJsx.push(
-            <LoginForm
+    const formJsx = [];
+    formJsx.push(
+        <LoginForm
             key={"loginForm"}
             changePassword={event => this.updatePassword(event)}
             changeEmail={event => this.updateEmail(event)}
@@ -71,36 +73,37 @@ class Home extends Component {
             password={this.state.password}
         />);
 
-        if (authToken === "error") {
-            formJsx.push(
-                <div key={"errorMsg"} className="page-header" style={this.col_md_12}>
-                    <small>Bad credential (email/password)!</small>
-                </div>
-            )
-        } else if (authToken) {
-            this.nextPath();
-        }
-
-        return (
-            <div>
-                {formJsx}
-            </div>
-        );
+    if (authToken === "error") {
+      formJsx.push(
+          <div key={"errorMsg"} className="page-header" style={this.col_md_12}>
+            <small>Bad credential (email/password)!</small>
+          </div>
+      )
     }
+    else if (authToken) {
+      this.nextPath();
+    }
+
+    return (
+        <div>
+          {formJsx}
+        </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const authToken = state.loginReducer.get(POPULATE_KEY_LOG_IN);
+  const authToken = state.loginReducer.get(POPULATE_KEY_LOG_IN);
 
-    return {
-        authToken
-    };
+  return {
+    authToken
+  };
 };
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        doLogin
-    }, dispatch);
+  return bindActionCreators({
+    doLogin
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/main/MainContent.js
+++ src/main/MainContent.js
@@ -3,26 +3,69 @@ import {doLogin} from "../redux/actions/loginActions";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import {POPULATE_KEY_LOG_IN} from "../redux/actions/constants";
+import Table from "react-bootstrap/Table"
 
 class MainContent extends Component {
-    render() {
-        return (
-            <div>Sample content</div>
-        )
-    }
+
+  generateHeader() {
+
+  }
+
+  generateContent() {
+
+  }
+
+  render() {
+
+    const tableJsx = (
+        <Table striped bordered hover>
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+          </tbody>
+        </Table>
+    );
+
+    return (
+        tableJsx
+    )
+  }
 }
 
 const mapStateToProps = state => {
-    const userAuth = state.loginReducer.get(POPULATE_KEY_LOG_IN);
-    return {
-        userAuth
-    };
+  const userAuth = state.loginReducer.get(POPULATE_KEY_LOG_IN);
+  return {
+    userAuth
+  };
 };
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        doLogin
-    }, dispatch);
+  return bindActionCreators({
+    doLogin
+  }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainContent);
